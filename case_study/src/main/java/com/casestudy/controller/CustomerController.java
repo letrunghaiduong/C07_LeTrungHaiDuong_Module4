7c76bbb9fe81c04102f7489681a86e38edb47ccb
letrunghaiduong <letrunghaiduong260701@gmail.com>
2022-12-08 08:25:35 +0700

    nopbai

--- case_study/src/main/java/com/casestudy/controller/CustomerController.java
+++ case_study/src/main/java/com/casestudy/controller/CustomerController.java
@@ -1,12 +1,16 @@
 package com.casestudy.controller;
 
+import com.casestudy.model.customer.Customer;
 import com.casestudy.service.customer.ICustomerService;
 import com.casestudy.service.customer.ICustomerTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/customer")
@@ -17,10 +21,28 @@ public class CustomerController {
     @Autowired
     private ICustomerTypeService customerTypeService;
 
-    @GetMapping("/customer")
+    @GetMapping("")
     public String customerList(Model model){
         model.addAttribute("customerList", customerService.findAll());
         model.addAttribute("customerTypeList", customerTypeService.findAll());
         return "customer/list";
     }
+
+
+    @GetMapping("/create")
+    public  String createCustomer(Model model) {
+        model.addAttribute("customerTypeList",customerTypeService.findAll());
+        model.addAttribute("customer", new Customer());
+        return "customer/create";
+    }
+
+
+    @PostMapping("/create")
+    public String save(@ModelAttribute("customer") Customer customer, RedirectAttributes redirectAttributes) {
+        customerService.save(customer);
+        redirectAttributes.addFlashAttribute("message", "New customer created successfully");
+        return "redirect:/customer";
+    }
+
+
 }
---
+++ ss13/spring-security/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("")
+    public String loginSuccess(){
+        return "login";
+    }
+}
